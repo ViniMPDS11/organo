5079c992a9beccf55d82013dcfe568635cbb82a2
ViniMPDS11 <vinimpdevs@gmail.com>
2024-07-05 21:46:46 -0300

    Segundo commit

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -2,7 +2,7 @@ import './Banner.css';
 function Banner() {
     return (
         <header className="banner">
-            <img src="/imagens/banner.png" alt="O banner principar do Organo"/>
+            <img src="/imagens/banner.png" alt="O banner principal do Organo"/>
         </header>
     )
 }
