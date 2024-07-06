1d46c8dc1a08788188a160a614f25f96c4d400ba
ViniMPDS11 <vinimpdevs@gmail.com>
2024-07-05 22:27:34 -0300

    Terceiro commit

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -4,12 +4,12 @@ const Rodape = (props) => {
     return (
         <div className='Rodape'>
             <div>
-                <img src='../../imagens/fb.png' alt='Logo do Facebook'/>
-                <img src='../../imagens/tw.png' alt='Logo do Twitter'/>
-                <img src='../../imagens/ig.png' alt='Logo do Instagram'/>
+                <img src='./imagens/fb.png' alt='Logo do Facebook'/>
+                <img src='./imagens/tw.png' alt='Logo do Twitter'/>
+                <img src='./imagens/ig.png' alt='Logo do Instagram'/>
             </div>
 
-            <img src='../../imagens/logo.png' alt='Logo da Alura' className='logoAlura'/>
+            <img src='./imagens/logo.png' alt='Logo da Alura' className='logoAlura'/>
 
             <p>Desenvolvido por Alura.</p>
         </div>
